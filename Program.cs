05b36b307e9952caeba47cd2b8535667684815e1
Maroc <sophiene.maroc@gmail.Com>
2020-02-01 15:51:09 +0100

    test

--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ namespace kataCombeSophiene
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             CoffeeMachine  coffeeMachine = new CoffeeMachine();
             coffeeMachine.TranslateCommand("Drink maker makes 1 tea with 1 sugar and a stick");
             Console.ReadLine();
